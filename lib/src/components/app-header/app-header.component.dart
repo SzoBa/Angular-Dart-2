1d18a53bf71d9076763d0bbe4442479fecaaf986
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-25 14:57:32 +0200

    Angular dart base app

---
+++ lib/src/components/app-header/app-header.component.dart
@@ -0,0 +1,23 @@
+// import 'dart:async';
+
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'app-header',
+  templateUrl: './app-header.template.html',
+  // styleUrls: ['component_path.css'],
+  directives: [coreDirectives],
+  providers: [],
+)
+
+class AppHeader {}
+
+// class AppHeader implements OnInit {
+
+//   @override
+//   Future<Null> ngOnInit() async {
+//     // TODO: implement ngOnInit
+//   }
+
+
+// }
