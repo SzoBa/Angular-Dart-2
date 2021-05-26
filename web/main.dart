15a1e5652463908a4a8949856f9b5f07d440fc5f
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-26 13:02:50 +0200

    Add routing to main.dart

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,16 @@
 import 'package:angular/angular.dart';
-import 'package:basicdemo/app_component.template.dart' as ng;
+import 'package:angular_router/angular_router.dart';
+
+import 'package:basicdemo/app_component.template.dart' as app;
+
+import 'main.template.dart' as self;
+
+@GenerateInjector(routerProvidersHash,)
+
+final InjectorFactory injector = self.injector$Injector;
 
 void main() {
-  runApp(ng.AppComponentNgFactory);
+  runApp(app.AppComponentNgFactory, createInjector: injector);
 }
+
+//injector.register(CustomFactory(() => Engine()));
