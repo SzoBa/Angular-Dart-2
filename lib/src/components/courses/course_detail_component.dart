e0308370e8ab3b6ef06dd7813b2580d1a65d6159
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-27 00:13:26 +0200

    Add course detail component dart

---
+++ lib/src/components/courses/course_detail_component.dart
@@ -0,0 +1,10 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'course-detail',
+  templateUrl: './course_detail_template.html',
+  directives: [coreDirectives],
+  providers: [],
+)
+class CourseDetail {
+}
