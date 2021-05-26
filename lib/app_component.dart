5e592d8b267eae3cac626779bac53fa80207141f
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-26 13:18:11 +0200

    Refactor App component

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,7 +1,6 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:basicdemo/src/components/header/header_component.dart';
-import 'package:basicdemo/src/components/courses/courses_component.dart';
 import 'package:basicdemo/src/utils/routes.dart';
 
 
@@ -12,7 +11,7 @@ import 'package:basicdemo/src/utils/routes.dart';
   selector: 'my-app',
   // styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [routerDirectives, AppHeader, CoursesComponent],
+  directives: [routerDirectives, AppHeader],
   exports: [RoutePaths, Routes]
 )
 class AppComponent {
