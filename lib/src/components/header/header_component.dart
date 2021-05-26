50f96c595c0554193882459a65fb1100071ffbd4
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-26 14:32:30 +0200

    Add exports to header dart

--- lib/src/components/header/header_component.dart
+++ lib/src/components/header/header_component.dart
@@ -1,13 +1,16 @@
 // import 'dart:async';
 
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
+import 'package:basicdemo/src/utils/route_paths.dart';
 
 @Component(
   selector: 'app-header',
   templateUrl: './header_template.html',
   // styleUrls: ['component_path.css'],
-  directives: [coreDirectives],
+  directives: [coreDirectives, routerDirectives],
   providers: [],
+  exports: [RoutePaths]
 )
 
 class AppHeader {}
