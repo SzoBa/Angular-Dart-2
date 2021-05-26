a552484321aa248ff09e076fc55df18484bff300
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-26 13:17:58 +0200

    Add home path to routes

--- lib/src/utils/route_paths.dart
+++ lib/src/utils/route_paths.dart
@@ -2,4 +2,5 @@ import 'package:angular_router/angular_router.dart';
 
 class RoutePaths {
   static final courses = RoutePath(path: 'courses');
+  static final home = RoutePath(path: '/');
 }
--- lib/src/utils/routes.dart
+++ lib/src/utils/routes.dart
@@ -4,6 +4,7 @@ import 'package:angular_router/angular_router.dart';
 import 'route_paths.dart';
 
 import '../components/courses/courses_component.template.dart' as courses_template;
+import '../components/home/home_component.template.dart' as home_template;
 
 export 'route_paths.dart';
 
@@ -11,6 +12,10 @@ class Routes {
   static final courses = RouteDefinition(
     routePath: RoutePaths.courses,
     component: courses_template.CoursesComponentNgFactory,
+  );
+  static final home = RouteDefinition(
+    routePath: RoutePaths.home,
+    component: home_template.HomeComponentNgFactory,
   );
 
   static final all = <RouteDefinition>[
