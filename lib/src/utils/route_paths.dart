f10c66b139756ab49c4917d593974fdf2bda8889
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-27 00:28:24 +0200

    Add course detail route

--- lib/src/utils/route_paths.dart
+++ lib/src/utils/route_paths.dart
@@ -1,6 +1,12 @@
 import 'package:angular_router/angular_router.dart';
 
+String idParam = 'id';
 class RoutePaths {
   static final courses = RoutePath(path: 'courses');
   static final home = RoutePath(path: '/');
+  static final course_detail = RoutePath(path: '${courses.path}/:$idParam');
 }
+
+// String getId(Map<String, String> params) {
+//   return params[idParam];
+// }
