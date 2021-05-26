c11c1f26d616deb88492e717fa052d65818cfaeb
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-26 13:04:59 +0200

    Add courses dart file

---
+++ lib/src/components/courses/courses_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'courses',
+  templateUrl: './courses_template.html',
+  // styleUrls: ['component_path.css'],
+  directives: [coreDirectives],
+  providers: [],
+)
+class CoursesComponent {
+}
