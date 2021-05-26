5b15fceac4f12821c888dc3a354457b6156a547c
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-27 00:14:10 +0200

    Add view details redirect to courses

--- lib/src/components/courses/courses_component.dart
+++ lib/src/components/courses/courses_component.dart
@@ -1,6 +1,8 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 import 'package:basicdemo/src/components/cards/card_component.dart';
 import 'package:basicdemo/src/model/course_model.dart';
+import 'package:basicdemo/src/utils/routes.dart';
 
 @Component(
   selector: 'courses',
@@ -10,6 +12,12 @@ import 'package:basicdemo/src/model/course_model.dart';
   providers: [],
 )
 class CoursesComponent {
+
+  final Router _router;
+
+  CoursesComponent(this._router);
+
+
   List<Course> courses = [
     Course(
       '111', 'The title1', 'Myself', 9.99, 'Fancy description', 5, 'assets/images/1.png', ['Lecture1', 'Lecture2'],
@@ -23,5 +31,13 @@ class CoursesComponent {
 
   void deleteItem(String uid) {
     courses.removeWhere((course) => course.uid == uid);
+  }
+
+  Future<NavigationResult> viewDetail(String uid) {
+    return _router.navigate(_redirectUrlMaker(uid));
+  }
+
+  String _redirectUrlMaker(String uid) {
+    return RoutePaths.course_detail.toUrl(parameters: {idParam: uid});
   }
 }
