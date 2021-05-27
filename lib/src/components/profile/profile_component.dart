1c401667576f0a71a9d4c9e70b9cf30226973774
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-28 00:08:05 +0200

    Add profile component dart

---
+++ lib/src/components/profile/profile_component.dart
@@ -0,0 +1,26 @@
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_router/angular_router.dart';
+import 'package:basicdemo/src/components/profile/profile_service.dart';
+
+@Component(
+  selector: 'profile',
+  templateUrl: './profile_template.html',
+  directives: [coreDirectives, routerDirectives, formDirectives],
+  providers: [ClassProvider(ProfileService)],
+  pipes: [commonPipes],
+)
+
+class ProfileComponent implements OnInit{
+  final ProfileService _profileService;
+  bool editMode = false;
+  Profile profile;
+
+  ProfileComponent(this._profileService);
+
+
+
+  @override
+  void ngOnInit() {
+    profile = _profileService.getProfile();
+  }}
