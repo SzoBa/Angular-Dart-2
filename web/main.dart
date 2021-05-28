61e49d93d8a5058e60b6e3bbd6218c2af8e03edc
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-29 01:25:30 +0200

    Add memory db mock

--- web/main.dart
+++ web/main.dart
@@ -2,10 +2,17 @@ import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 
 import 'package:basicdemo/app_component.template.dart' as app;
+import 'package:basicdemo/src/utils/in-memory-data.service.dart';
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
 
 import 'main.template.dart' as self;
 
-@GenerateInjector(routerProvidersHash,)
+@GenerateInjector([
+  routerProvidersHash,
+  ClassProvider(Client, useClass: BrowserClient),
+  ClassProvider(Client, useClass: InMemoryData)
+])
 
 final InjectorFactory injector = self.injector$Injector;
 
